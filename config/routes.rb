20e52e0d121bddfafff9f09ee83c375f2bd7c4f1
hurik zamith <hurikzamith@gmail.com>
2023-04-13 18:30:09 -0300

    alt root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  root 'api/v1/movies#read_csv'
-
-  namespace 'api' do
-    namespace 'v1' do
-      resources :movies, only: [:index]
-    end
-  end
-
-  # get 'api/v1/movies/read_csv'
-
+  root to: 'api/v1/movies#index'
+  get 'api/v1/movies/read_csv'
+  # namespace 'api' do
+  #   namespace 'v1' do
+  #     resources :movies, only: [:index]
+  #   end
+  # end
 end
