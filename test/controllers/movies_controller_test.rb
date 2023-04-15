777d117a1f19b9e9e07ac918e2d24a5be4053662
hurik zamith <hurikzamith@gmail.com>
2023-04-15 14:19:53 -0300

    added tests to movie_controller test

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,13 +1,27 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionDispatch::IntegrationTest
-  test "should get movies index" do
-    get api_v1_movies_path
+
+  test "should get index" do
+    get api_v1_movies_url
     assert_response :success
+    assert_equal response.parsed_body.keys, ['movies', 'current_page', 'total_pages', 'total_count']
   end
 
-  test "should get csv url" do
-    get read_csv_api_v1_movies_path
+  test "should get filtered movies" do
+    Movie.create(title: 'The Matrix', year: '1999')
+    Movie.create(title: 'Inception', year: '2010')
+
+    get api_v1_movies_url, params: { year: '1999' }
     assert_response :success
+    assert_equal response.parsed_body['movies'].size, 1
+    assert_equal response.parsed_body['movies'][0]['title'], 'The Matrix'
+
+    get api_v1_movies_url, params: { title: 'Inception' }
+    assert_response :success
+    assert_equal response.parsed_body['movies'].size, 1
+    assert_equal response.parsed_body['movies'][0]['title'], 'Inception'
   end
+
+
 end
