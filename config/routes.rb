c69f9c2bc79c5f9025bdc3b8cfff1ab8cd847069
hurik zamith <hurikzamith@gmail.com>
2023-04-15 14:33:38 -0300

    filtering tests added movies controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-Rails.application.routes.draw do
-  namespace 'api' do
-    namespace 'v1' do
-      resources :movies, only: [:index] do
-        get 'read_csv', on: :collection
+  Rails.application.routes.draw do
+    namespace 'api' do
+      namespace 'v1' do
+        resources :movies, only: [:index] do
+          get 'read_csv', on: :collection
+        end
       end
     end
   end
-end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class MoviesControllerTest < ActionDispatch::IntegrationTest
 
+  setup do
+    Movie.create(title: 'The Matrix', year: '1999', genre: 'Action', country: 'United States', published_at: '1999-03-31', description: 'A computer hacker..')
+    Movie.create(title: 'Inception', year: '2010', genre: 'Action', country: 'United States', published_at: '2010-07-16', description: 'A thief who steals corporate secrets ..')
+  end
+
   test "should get index" do
     get api_v1_movies_url
     assert_response :success
@@ -9,9 +14,6 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get filtered movies" do
-    Movie.create(title: 'The Matrix', year: '1999')
-    Movie.create(title: 'Inception', year: '2010')
-
     get api_v1_movies_url, params: { year: '1999' }
     assert_response :success
     assert_equal response.parsed_body['movies'].size, 1
@@ -21,6 +23,19 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal response.parsed_body['movies'].size, 1
     assert_equal response.parsed_body['movies'][0]['title'], 'Inception'
+  end
+
+  test "should filter movies by genre" do
+    get api_v1_movies_url, params: { genre: 'Action' }
+    assert_response :success
+    assert_equal response.parsed_body['movies'][0]['genre'], 'Action'
+  end
+
+
+  test "should filter movies by country" do
+    get api_v1_movies_url, params: { country: 'United States' }
+    assert_response :success
+    assert_equal response.parsed_body['movies'][0]['country'], 'United States'
   end
 
 
